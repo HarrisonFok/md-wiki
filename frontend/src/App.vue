850d7e18eb9e3fae29a81d7aea83e21ab73d10bd
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-04-17 18:28:50 -0400

    Configured it to allow cross-origin requests; Displaying all the articles now

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,18 @@
 <template>
   <div id="app">
-    {{ article }}
+    <table class="table">
+      <thead>
+        <th>Name</th>
+        <th>Content</th>
+      </thead>
+      <tbody>
+        <!-- @dblclick="$data.student = student" -->
+        <tr v-for="article in articles" :key="article.id">
+          <td>{{ article.name }}</td>
+          <td>{{ article.content }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,8 +21,12 @@ export default {
   name: 'App',
   data() {
     return {
-      article: "Content"
+      articles: []
     }
+  },
+  async created() {
+    let res = await fetch("http://localhost:8000/articles/")
+    this.articles = await res.json()
   }
 }
 </script>
