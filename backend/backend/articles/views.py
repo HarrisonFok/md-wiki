a9dc6c50976641da277f48608fbd6fd1ebfac037
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-04-28 12:41:37 -0400

    Reinitialized Vue app to include routing

--- backend/backend/articles/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.viewsets import ModelViewSet
-
-from .models import Article
-from .serializers import ArticleSerializer
-
-class ArticlesViewSet(ModelViewSet):
-    queryset = Article.objects.all()
-    serializer_class = ArticleSerializer
