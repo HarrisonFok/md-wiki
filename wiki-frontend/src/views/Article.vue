18c8c6fc6ae42c8bcee5868df62a4660b01837d0
HarrisonFok <pak.fok@mail.utoronto.ca>
2021-04-28 13:46:27 -0400

    Added error checking for the article viewing page

--- wiki-frontend/src/views/Article.vue
+++ wiki-frontend/src/views/Article.vue
@@ -1,10 +1,17 @@
 <template>
   <div>
-    <router-link :to="'/edit/' + article.name">
-        <div class="btn btn-outline-info btn-sm mx-1">Edit</div>
-    </router-link>
-    <h1>{{ this.article.name }}</h1>
-    <p>{{ this.article.content }}</p>
+    <div v-if="this.error == null">
+        <router-link :to="'/edit/' + article.name">
+            <div class="btn btn-outline-info btn-sm mx-1">Edit</div>
+        </router-link>
+        <h1>{{ this.article.name }}</h1>
+        <p>{{ this.article.content }}</p>
+    </div>
+    <div v-else>
+        <h3>
+            No article with this exact name found. Use Edit button in the header to add it.
+        </h3>
+    </div>
   </div>
 </template>
 
@@ -13,15 +20,21 @@ export default {
     name: 'Article',
     data() {
         return {
-            article: {}
+            article: {},
+            error: null
         }
     },
     async created() {
         let name = this.$route.params.name
         let res = await fetch(`http://localhost:8000/articles/${name}`)
+        console.log(res)
         let content = await res.json()
-        this.article = { name, content}
-        console.log(this.article)
+        let checkError = {"detail": "Not Found"}
+        if (!res.ok) {
+            this.error = checkError
+        } else {
+            this.article = { name, content }
+        }
     }
 }
 </script>
